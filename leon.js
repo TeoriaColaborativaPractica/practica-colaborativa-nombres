6871dc0e9fbc658fea833a6031a34f468343222e
Yamil Ayma <yamilaymadev@gmail.com>
2025-04-09 00:40:48 -0500

    Update leon,js

---
+++ leon.js
@@ -0,0 +1,9 @@
+
+const datos_leon = { 
+    nombre: 'Gabriel Omar', 
+    apellido: 'Leon Rojas', 
+    id: '1488236', 
+    direccion: 'calle Ulises mr 175 lt 18', 
+};
+
+cloneTemplate(datos_leon);
